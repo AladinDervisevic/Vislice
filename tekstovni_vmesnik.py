57da922b5092f55d5366e8a6dc5547de170be659
Aladin Dervisevic <aladin77.ad@protonmail.com>
2020-05-12 15:38:44 +0200

    Naredil sem kostur spletnega vmesnika

---
+++ tekstovni_vmesnik.py
@@ -0,0 +1,16 @@
+from model import Igra
+
+def izpis_igre(igra):
+    pass
+
+def izpis_zmage(igra):
+    pass
+
+def izpis_poraza(igra):
+    pass
+
+def zahtevaj_vnos():
+    pass
+
+def pozeni_vmesnik():
+    pass
